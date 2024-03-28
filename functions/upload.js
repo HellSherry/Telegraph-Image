2809f6a79d1f641fc8f1006c158f860fd31c9096
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-03-28 17:32:12 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -38,5 +38,12 @@ export async function onRequest(context) {
         body: request.body,
     });
   // 设置允许跨域的域名
-     return response;
+     return new Response(response.body, {
+            status: 200, // 空内容响应，表示已成功处理请求但无内容返回
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': '*', // 或者明确指定允许的请求头
+            },
+        });;
 }
