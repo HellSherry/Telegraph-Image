659710e8e3660e34e61af60db720894200d09181
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-04-22 00:53:53 +0800

    Update ajax.js

--- functions/ajax.js
+++ functions/ajax.js
@@ -15,7 +15,7 @@ export async function onRequest(context) {
     // 允许的请求头
     request.headers.set('Access-Control-Allow-Headers', '*'); // 或者明确指定允许的请求头
 
-     return new Response(request, {
+     return new Response(JSON.stringify(request), {
             status: 200, // 空内容响应，表示已成功处理请求但无内容返回
             headers: {
                 'Access-Control-Allow-Origin': '*',
