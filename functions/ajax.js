ce9e2d17e9b4531f23b73ec1b4649fdfa9a96f38
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-04-22 01:12:56 +0800

    Update ajax.js

--- functions/ajax.js
+++ functions/ajax.js
@@ -2,7 +2,7 @@ export async function onRequest(req, res) {
  
  
 
-     return new Response(    req
+     return new Response(    req.query
 , {
             status: 200, // 空内容响应，表示已成功处理请求但无内容返回
             headers: {
