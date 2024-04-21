d60e398f6eb53a88609299c63a9fd0ad15bfd1fd
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-04-22 01:10:43 +0800

    Update ajax.js

--- functions/ajax.js
+++ functions/ajax.js
@@ -1,21 +1,8 @@
-export async function onRequest(context) {
-    const {
-        request,
-        env,
-        params,
-        waitUntil,
-        next,
-        data,
-    } = context;
+export async function onRequest(req, res) {
+ 
+ 
 
-    // 设置允许跨域的域名
-    request.headers.set('Access-Control-Allow-Origin', '*');
-    // 允许的 HTTP 方法
-    request.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    // 允许的请求头
-    request.headers.set('Access-Control-Allow-Headers', '*'); // 或者明确指定允许的请求头
-
-     return new Response(    params.url
+     return new Response(    req
 , {
             status: 200, // 空内容响应，表示已成功处理请求但无内容返回
             headers: {
