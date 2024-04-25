dca2cadc80537c2f44910b1f656ef93d47ac7c06
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-04-26 01:43:03 +0800

    Update gettoken.js

--- functions/gettoken.js
+++ functions/gettoken.js
@@ -11,9 +11,9 @@ export async function onRequest(context) {
 
 
 const response = await fetch('https://iot.diandeng.tech/api/v1/user/login?username=19174143998&password=efefe890e21c0c4dcde96e4addcf20cbe577759b078154e6ecf204fd45e54afc');
-const data = await response.json();
+const data2 = await response.json();
   
-  const response1 = await fetch(`https://iot.diandeng.tech/api/v1/user/overview?uuid=${data.detail.uuid}&token=${data.detail.token}`);
+  const response1 = await fetch(`https://iot.diandeng.tech/api/v1/user/overview?uuid=${data2.detail.uuid}&token=${data2.detail.token}`);
 const data1 = await response1.json();
     // 非预检请求，则进行转发
 
