c8182739ed0d9d00300125e70ca2ee31fc4ab31d
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-11-12 20:09:22 +0800

    Update getbyqution.js

--- functions/getbyqution.js
+++ functions/getbyqution.js
@@ -12,7 +12,7 @@ export async function onRequest(context) {
 
 const response = await fetch('http://www.sqlpub.com:8081/sql/execute?sql=select%20*%20from%20cslg%20where%20questions%20如图%20%27%25hello%25%27&dbId=mysql_mysql_sqlpub_com_3306_sherrycslg_sherrycslg&token=be21d52c97854653b882fcc3bdaadd64
 ');
-const data1 = await response.json();
+const data1 = await response.text();
   
 
   
