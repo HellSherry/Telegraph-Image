19a44ba9ae3d2252cba13b7c60c64da932586bd9
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-11-12 16:54:17 +0800

    Update getbyqution.js

--- functions/getbyqution.js
+++ functions/getbyqution.js
@@ -8,15 +8,10 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
-    const bing_data = await res.json();
-    const return_data={
-        "status":true,
-        "message":"操作成功",
-        "data": bing_data.images
-    }
-    const info = JSON.stringify(return_data);
-  return new Response( "good time"+params, {
+   //// const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
+   // const bing_data = await res.json();
+  
+  return new Response( "good time"+ JSON.stringify(params) , {
            status: 200, // 空内容响应，表示已成功处理请求但无内容返回
            headers: {
                'Access-Control-Allow-Origin': '*',
