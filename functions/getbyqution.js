7022ae366a585913f85d5aeb8f8f67898711f425
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-11-12 19:46:38 +0800

    Update getbyqution.js

--- functions/getbyqution.js
+++ functions/getbyqution.js
@@ -10,20 +10,7 @@ export async function onRequest(context) {
     } = context;
    //// const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
    // const bing_data = await res.json();
-  const res =   fetch("http://www.sqlpub.com:8081/sql/execute?sql=select%20*%20from%20cslg%20where%20questions%20like%20%27%25"+request.body+"%25%27&dbId=mysql_mysql_sqlpub_com_3306_sherrycslg_sherrycslg&token=be21d52c97854653b882fcc3bdaadd64", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "x-requested-with": "XMLHttpRequest",
-    "cookie": "_tokensu=be21d52c97854653b882fcc3bdaadd64; loginCacheMysql={\"db\":{\"f621bb7317bf7bc2e306f8c3fce6abfd\":{\"host\":\"d0c1bXlzcWwyuc3FscHViQLmNvbQ==\",\"port\":\"fe21MzMwnhNi1XHUm3uIYjaNg==\",\"name\":\"00d1c2hlcnJyMFD5Y3NsZwQMa4==\",\"userName\":\"4331c2hlzjm7QcT2ncnJ5Y3NsZw=mOXVZV=\",\"password\":\"f641S01maBPDZqSWYyS3BXRmNROvQ==\",\"id\":\"f621bb7317bf7bc2e306f8c3fce6abfd\"}}}",
-    "Referer": "http://www.sqlpub.com:8081/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-  },
-  "body": null,
-  "method": "GET"
-});
+  const res =   fetch("http://www.sqlpub.com:8081/sql/execute?sql=select%20*%20from%20cslg%20where%20questions%20like%20%27%25"+request.body+"%25%27&dbId=mysql_mysql_sqlpub_com_3306_sherrycslg_sherrycslg&token=be21d52c97854653b882fcc3bdaadd64");
   const bing_data = await res.json();
   return new Response( bing_data  , {
            status: 200, // 空内容响应，表示已成功处理请求但无内容返回
