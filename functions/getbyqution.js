bb071ae2b6f154571e44f3c5e81087311e6f9296
HellSherry <114070772+HellSherry@users.noreply.github.com>
2024-11-12 20:28:05 +0800

    Update getbyqution.js

--- functions/getbyqution.js
+++ functions/getbyqution.js
@@ -10,14 +10,15 @@ export async function onRequest(context) {
     } = context;
    //// const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
    // const bing_data = await res.json();
-  const res = await  fetch("http://www.sqlpub.com:8081/sql/execute?sql=select%20*%20from%20cslg%20where%20questions%20like%20%27%25%25%27&dbId=mysql_mysql_sqlpub_com_3306_sherrycslg_sherrycslg&token=be21d52c97854653b882fcc3bdaadd64");
-  const bing_data = await res.json();
-  return new Response( bing_data  , {
-           status: 200, // 空内容响应，表示已成功处理请求但无内容返回
-           headers: {
-               'Access-Control-Allow-Origin': '*',
-               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-               'Access-Control-Allow-Headers': '*', // 或者明确指定允许的请求头
-           },
-       });
+     const res =  await fetch("http://www.sqlpub.com:8081/sql/execute?sql=select%20*%20from%20cslg%20where%20questions%20like%20%27%25%25%27&dbId=mysql_mysql_sqlpub_com_3306_sherrycslg_sherrycslg&token=be21d52c97854653b882fcc3bdaadd64");
+    const bing_data = await res.text();
+    return new Response( bing_data  , {
+             status: 200, // 空内容响应，表示已成功处理请求但无内容返回
+             headers: {
+                 'Access-Control-Allow-Origin': '*',
+                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                 'Access-Control-Allow-Headers': '*', // 或者明确指定允许的请求头
+             },
+         });
+ 
   }
